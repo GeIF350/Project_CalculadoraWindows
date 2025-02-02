efd41998d87f14fcd94dbaab53bfec1b29187751
GeIF350 <d.geovana@estudante.ifmt.edu.br>
2025-02-02 15:15:14 -0400

    finalização do código back-end

--- js/script.js
+++ js/script.js
@@ -9,9 +9,9 @@ class Calculator {
         this.currentOperation = "";
     }
 
-    //add digit to calculator screen
+    //Add digit to calculator screen
     addDigit(digit){
-        //check if current operation already has a dot
+        //Check if current operation already has a dot
         if(digit == "." && this.currentOperationText.innerText.includes(".")){
             return;
         }
@@ -20,15 +20,110 @@ class Calculator {
         this.updateScreen();
     }
 
-    //process all operations calculator
+    //Process all operations calculator
     processOperation(operation) {
-        console.log(operation);
-        
+        // Check if current value is empty
+        if(this.currentOperationText.innerText === "" && operation !== "C") {
+            // Change operation
+            if(this.previousOperationText.innerText !== "") {
+                this.changeOperation(operation);
+            }
+            return;
+        }
+
+        //Get current and previous value
+        let operationValue;
+        const previous = +this.previousOperationText.innerText.split(" ")[0];
+        const current = +this.currentOperationText.innerText;
+
+        switch(operation) {
+            case "+":
+                operationValue = previous + current;
+                this.updateScreen(operationValue, operation, current, previous);
+                break;
+            case "-":
+                operationValue = previous - current;
+                this.updateScreen(operationValue, operation, current, previous);
+                break;
+            case "*":
+                operationValue = previous * current;
+                this.updateScreen(operationValue, operation, current, previous);
+                break;
+            case "/":
+                operationValue = previous / current;
+                this.updateScreen(operationValue, operation, current, previous);
+                break;
+            case "DEL":
+                this.processDelOperation();
+                break;
+            case "CE":
+                this.processClearCurrentOperation();
+                break;
+            case "C":
+                this.processClearOperation();
+                break;
+            case "=":
+                this.processEqualOperator();
+                break;
+            default:
+                return;
+        }
     }
 
-    //update values of the calculator screen
-    updateScreen(){
-        this.currentOperationText.innerText += this.currentOperation;
+    //Update values of the calculator screen
+    updateScreen(
+        operationValue = null,
+        operation = null, 
+        current = null,
+        previous = null){
+
+        console.log();
+            
+        if (operationValue === null) {
+            this.currentOperationText.innerText += this.currentOperation;
+        } else {
+            // Check if value is zero, if it is just add current value
+            if (previous === 0) {
+                operationValue = current;
+            }
+
+            // Add current value to previous
+            this.previousOperationText.innerText = `${operationValue} ${operation}`
+            this.currentOperationText.innerText = "";
+        }
+    }
+
+    // Change math operation
+    changeOperation(operation) {
+
+        const mathOperations = ["*", "/", "-", "+"]
+        if(!mathOperations.includes(operation)) {
+            return;
+        }
+
+        this.previousOperationText.innerText =  this.previousOperationText.innerText.slice(0, -1) + operation;
+    }
+
+    // Delete the last digit
+    processDelOperation() {
+        this.currentOperationText.innerText = this.currentOperationText.innerText.slice(0, -1);
+    }
+
+    // Clear current operation
+    processClearCurrentOperation() {
+        this.currentOperationText.innerText = "";
+    }
+
+    // Clear all operations
+    processClearOperation() {
+        this.currentOperationText.innerText = "";
+        this.previousOperationText.innerText = "";
+    }
+
+    // Process an operation
+    processEqualOperator() {
+        const operation = previousOperationText.innerHTML.split(" ")[1];
+        this.processOperation(operation);
     }
 }
 
